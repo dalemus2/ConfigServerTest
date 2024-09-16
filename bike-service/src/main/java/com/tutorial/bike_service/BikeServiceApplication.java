5f03414245a1d290136059957883def3a89d63fb
dalemus2 <danielcabrera1090@gmail.com>
2024-09-15 20:10:16 -0600

    Finalizando config server

--- bike-service/src/main/java/com/tutorial/bike_service/BikeServiceApplication.java
+++ bike-service/src/main/java/com/tutorial/bike_service/BikeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.tutorial.bike_service;
+	package com.tutorial.bike_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
